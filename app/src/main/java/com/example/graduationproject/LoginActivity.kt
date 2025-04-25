fe18daa98e778e0b9b53fa914c4398d853e5561f
Mohamed-Hussein-Dev <mohamd.h.yousuf@gmail.com>
2025-04-26 01:23:06 +0300

    add beso page

--- app/src/main/java/com/example/graduationproject/LoginActivity.kt
+++ app/src/main/java/com/example/graduationproject/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.example.graduationproject
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
@@ -16,14 +18,26 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-       var etName = findViewById<TextInputEditText>(R.id.et_name)
-       var btnLogin = findViewById<MaterialButton>(R.id.btn_login)
+        var etName = findViewById<TextInputEditText>(R.id.et_name)
+        var btnLogin = findViewById<MaterialButton>(R.id.btn_login)
+        var besobtn = findViewById<Button>(R.id.beso)
+
+
+        besobtn.setOnClickListener{
+            val intent = Intent(this, Devolopers_Display::class.java)
+
+            startActivity(intent)
+        }
 
         btnLogin.setOnClickListener {
             val username = etName.text.toString()
-            val intent = Intent(this, CarSelectionActivity::class.java)
-            intent.putExtra("USERNAME", username)
-            startActivity(intent)
+            if(username != "" && username != null){
+                val intent = Intent(this, CarSelectionActivity::class.java)
+                intent.putExtra("USERNAME", username)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, "Please Enter Your name", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
