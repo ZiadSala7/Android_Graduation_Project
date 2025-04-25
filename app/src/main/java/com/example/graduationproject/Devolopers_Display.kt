3d2ff83048fe3ee0d1a562f06c870e8edb4b9fbd
Mohamed-Hussein-Dev <mohamd.h.yousuf@gmail.com>
2025-04-26 02:07:16 +0300

    shit

--- app/src/main/java/com/example/graduationproject/Devolopers_Display.kt
+++ app/src/main/java/com/example/graduationproject/Devolopers_Display.kt
@@ -1,6 +1,9 @@
 package com.example.graduationproject
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Devolopers_Display : AppCompatActivity() {
@@ -8,6 +11,15 @@ class Devolopers_Display : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_devolopers_information)
-        
+
+        var githupRepo = findViewById<Button>(R.id.Repo_link)
+
+        githupRepo.setOnClickListener{
+            val browserisfds = Intent(Intent.ACTION_VIEW)
+            browserisfds.data = Uri.parse("https://github.com/ZiadSala7/Android_Graduation_Project")
+
+            startActivity(browserisfds)
+
+        }
     }
 }
